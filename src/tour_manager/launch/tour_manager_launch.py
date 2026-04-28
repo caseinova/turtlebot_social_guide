0639c91b348e67cab27ba0c3eb63560447562771
caseino1 <a.liver.f.let.cha@gmail.com>
2026-04-28 12:03:38 +1000

    Create tour creator and integrate it with the system. Add launch file

---
+++ src/tour_manager/launch/tour_manager_launch.py
@@ -0,0 +1,18 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    return LaunchDescription([
+        Node(
+            package='tour_manager',
+            namespace='',
+            executable='tour_manager',
+            name='tour_manager'
+        ),
+        Node(
+            package='tour_manager',
+            namespace='',
+            executable='tour_saver',
+            name='tour_saver'
+        )
+    ])
--- src/tour_manager/setup.py
+++ src/tour_manager/setup.py
@@ -1,4 +1,6 @@
 from setuptools import find_packages, setup
+from glob import glob
+import os
 
 package_name = 'tour_manager'
 
@@ -10,6 +12,7 @@ setup(
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
+        (os.path.join('share', package_name, 'launch'), glob(os.path.join('launch', '*launch.[pxy][yma]*')))
     ],
     install_requires=['setuptools'],
     zip_safe=True,
@@ -24,7 +27,8 @@ setup(
     },
     entry_points={
         'console_scripts': [
-            'tour_manager = tour_manager.tour_manager_service:main'
+            'tour_manager = tour_manager.tour_manager_service:main',
+            'tour_saver = tour_manager.tour_saver:main'
         ],
     },
 )
